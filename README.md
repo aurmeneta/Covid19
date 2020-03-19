cee3a3e426e3d19f80de49e6774a1ef0a6a65735
Andrés Urmeneta <andresub009@gmail.com>
2020-03-18 22:02:29 -0300

    Version 1.0.3 Uses require instead of import Fix in readme

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 NPM Package to retrieve worldometers' live statistics of the Covid-19 pandemic. Scraper of [worldometers](https://www.worldometers.info).
 
 # Disclaimer
-This packages uses web scraping to retrieve the statistical information. This could potentially mean a copyright infringement and be criminally prosecuted. The scraped information belongs to Worldometers and its sources.
+This packages uses web scraping to retrieve the statistical information. This could potentially mean a copyright infringement and be criminally prosecuted. The scraped information belongs to Worldometers and its sources. Use by your own risk.
 
 # Usage
 Just import the module and run `getStats()`.
@@ -13,7 +13,7 @@ const covid19 = requiere('covid19-stats');
 let stats = await covid19.getStats();
 ```
 
-`getStats` returns an array containing the stats by country and the totals. The countries are named as listed in worldometers. Use by your own risk.
+`getStats` returns an array containing the stats by country and the totals. The countries are named as listed in worldometers.
 
 ```javascript
 [
--- index.js
+++ index.js
@@ -18,55 +18,57 @@ OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
 const siteUrl = "https://www.worldometers.info/coronavirus/#countries";
 const charsToRemove = /[,+]/g;
 
-import { get } from "axios";
-import { load } from "cheerio";
+const axios = require('axios');
+const cheerio = require('cheerio');
 
-export function getStats() { 	return get(siteUrl).then(response => {
-	let data = []; // Initialize an empty array to save the information to be retrieved.
+export function getStats() { 	
+	return axios.get(siteUrl).then(response => {
+		let data = []; // Initialize an empty array to save the information to be retrieved.
 
-	const $ = load(response.data); // Load the webpage.
-	const table = $('#main_table_countries_today'); // Look for the table.
-	const tbodies = table.find('tbody'); // Look for the 'tbody' tags in the table.
+		const $ = cheerio.load(response.data); // Load the webpage.
+		const table = $('#main_table_countries_today'); // Look for the table.
+		const tbodies = table.find('tbody'); // Look for the 'tbody' tags in the table.
 
-	// Data in the table is divided in two 'tbody' tags. The first one contains the information for each country and the second one the totals.
-	const tbody_countries = tbodies[0]; // Countries tbody.
-	const tbody_total = tbodies[1]; // Total tbody.
+		// Data in the table is divided in two 'tbody' tags. The first one contains the information for each country and the second one the totals.
+		const tbody_countries = tbodies[0]; // Countries tbody.
+		const tbody_total = tbodies[1]; // Total tbody.
 
-	const table_rows_countries = $(tbody_countries).find('tr').toArray();
-	const table_rows_total = $(tbody_total).find('tr');
+		const table_rows_countries = $(tbody_countries).find('tr').toArray();
+		const table_rows_total = $(tbody_total).find('tr');
 
-	// Get the totals.
-	const total_columns = $(table_rows_total[0]).find('td');
-	let country = 'Total';
-	let totalCases = parseInt($(total_columns[1]).text().replace(charsToRemove,'')) || 0;
-	let newCases = parseInt($(total_columns[2]).text().replace(charsToRemove,'')) || 0;
-	let totalDeaths = parseInt($(total_columns[3]).text().replace(charsToRemove,'')) || 0;
-	let newDeaths = parseInt($(total_columns[4]).text().replace(charsToRemove,'')) || 0;
-	let totalRecovered = parseInt($(total_columns[5]).text().replace(charsToRemove,'')) || 0;
-	let activeCases = parseInt($(total_columns[6]).text().replace(charsToRemove,'')) || 0;
-	let criticalCases = parseInt($(total_columns[7]).text().replace(charsToRemove,'')) || 0;
-	let ratio = parseFloat($(total_columns[8]).text().replace(charsToRemove,'')) || 0;
-	
-	// Add the date to the array.
-	data.push({country, totalCases, newCases, totalDeaths, newDeaths, totalRecovered, activeCases, criticalCases, ratio});
-
-	// Get the data by country.
-	table_rows_countries.forEach(row => {
-		const columns = $(row).find('td');
-		country = $(columns[0]).text().trim();
-		totalCases = parseInt($(columns[1]).text().replace(charsToRemove,'')) || 0;
-		newCases = parseInt($(columns[2]).text().replace(charsToRemove,'')) || 0;
-		totalDeaths = parseInt($(columns[3]).text().replace(charsToRemove,'')) || 0;
-		newDeaths = parseInt($(columns[4]).text().replace(charsToRemove,'')) || 0;
-		totalRecovered = parseInt($(columns[5]).text().replace(charsToRemove,'')) || 0;
-		activeCases = parseInt($(columns[6]).text().replace(charsToRemove,'')) || 0;
-		criticalCases = parseInt($(columns[7]).text().replace(charsToRemove,'')) || 0;
-		ratio = parseFloat($(columns[8]).text().replace(charsToRemove,'')) || 0;
-
-		// Add the data to the array.
+		// Get the totals.
+		const total_columns = $(table_rows_total[0]).find('td');
+		let country = 'Total';
+		let totalCases = parseInt($(total_columns[1]).text().replace(charsToRemove,'')) || 0;
+		let newCases = parseInt($(total_columns[2]).text().replace(charsToRemove,'')) || 0;
+		let totalDeaths = parseInt($(total_columns[3]).text().replace(charsToRemove,'')) || 0;
+		let newDeaths = parseInt($(total_columns[4]).text().replace(charsToRemove,'')) || 0;
+		let totalRecovered = parseInt($(total_columns[5]).text().replace(charsToRemove,'')) || 0;
+		let activeCases = parseInt($(total_columns[6]).text().replace(charsToRemove,'')) || 0;
+		let criticalCases = parseInt($(total_columns[7]).text().replace(charsToRemove,'')) || 0;
+		let ratio = parseFloat($(total_columns[8]).text().replace(charsToRemove,'')) || 0;
+		
+		// Add the date to the array.
 		data.push({country, totalCases, newCases, totalDeaths, newDeaths, totalRecovered, activeCases, criticalCases, ratio});
-	});
 
-	// Return the array.
-	return data;
-}); 	}
+		// Get the data by country.
+		table_rows_countries.forEach(row => {
+			const columns = $(row).find('td');
+			country = $(columns[0]).text().trim();
+			totalCases = parseInt($(columns[1]).text().replace(charsToRemove,'')) || 0;
+			newCases = parseInt($(columns[2]).text().replace(charsToRemove,'')) || 0;
+			totalDeaths = parseInt($(columns[3]).text().replace(charsToRemove,'')) || 0;
+			newDeaths = parseInt($(columns[4]).text().replace(charsToRemove,'')) || 0;
+			totalRecovered = parseInt($(columns[5]).text().replace(charsToRemove,'')) || 0;
+			activeCases = parseInt($(columns[6]).text().replace(charsToRemove,'')) || 0;
+			criticalCases = parseInt($(columns[7]).text().replace(charsToRemove,'')) || 0;
+			ratio = parseFloat($(columns[8]).text().replace(charsToRemove,'')) || 0;
+
+			// Add the data to the array.
+			data.push({country, totalCases, newCases, totalDeaths, newDeaths, totalRecovered, activeCases, criticalCases, ratio});
+		});
+
+		// Return the array.
+		return data;
+	}); 	
+}
