bd5cdced29db3e452da801ba2dd4873a82f89fe8
Andrés Urmeneta <andresub009@gmail.com>
2021-05-14 17:12:53 -0400

    added test for getCountry

--- test/test.js
+++ test/test.js
@@ -6,21 +6,10 @@ testGetStats()
     .then( () => {
         console.log("getStats passed!")
         return testGetCountry()
+    }).then( () => {
+        console.log("getCountry passed!")
     }).catch(e => {
         console.log(e)
         console.log("Test failed!")
         process.exit(1)
     })
-
-/*
-// getCountry
-(async () => {
-    console.log("Checking getCountry...")
-    for (country in countries) {
-        let stats = await covid19Stats.getCountry(country);
-        if (!stats) {
-            console.error(country + "missing!")
-            throw "There are missing countries! Check log."
-        }
-    }
-})()*/
